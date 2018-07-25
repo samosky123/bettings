b8e5c3b48dd65de89c5f9217f5b28bb2410210fc
Nguyen Vinh Tuan <tuan.nguyen@ascendcorp.com>
2018-07-25 16:29:24 +0700

    Initialize project

---
+++ bettings/users/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
+from django.contrib.auth import get_user_model
+
+from bettings.users.forms import UserChangeForm, UserCreationForm
+
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(auth_admin.UserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    fieldsets = (("User", {"fields": ("username",)}),) + auth_admin.UserAdmin.fieldsets
+    list_display = ["username", "is_superuser"]
+    search_fields = ["name"]
